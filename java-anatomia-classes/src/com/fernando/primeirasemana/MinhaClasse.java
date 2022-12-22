67f88234726294e9a349f33ab44ff9afa1466b7e
FernandoKain <fernandomanga@hotmail.com>
2022-12-22 08:06:49 -0300

    Operadores foram incluídos

---
+++ java-anatomia-classes/src/com/fernando/primeirasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package com.fernando.primeirasemana;
+public class MinhaClasse {
+    
+public static void main(String [] args) {
+    
+    String primeiroNome = "Fernando";
+    String segundoNome = "Cunha";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
