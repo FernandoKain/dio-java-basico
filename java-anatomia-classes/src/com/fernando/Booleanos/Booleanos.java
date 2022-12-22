6bd46ca5510ed3a469db738da0941fcd470cf29b
FernandoKain <fernandomanga@hotmail.com>
2022-12-22 08:27:38 -0300

    Booleanos foram inseridos

---
+++ java-anatomia-classes/src/com/fernando/Booleanos/Booleanos.java
@@ -0,0 +1,16 @@
+package com.fernando.Booleanos;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+public class Booleanos {
+    public static void main(String[] args) {
+        
+        boolean variavel = true;
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+        System.out.println(!variavel);
+
+    }
+}
---
+++ java-anatomia-classes/src/com/fernando/TiposDeVariaveis/TiposDeVariaveis.java
@@ -0,0 +1,19 @@
+package com.fernando.TiposDeVariaveis;
+
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        //A variável não é afetada.
+        int n1 = 5;
+        System.out.println(- n1);// -5
+        System.out.println(n1);// 5
+        
+        //A variável é afetada
+        int n2 = 4;
+        n2 = -n2;
+        System.out.println(n2); //-4
+
+        //Tornando a variável positiva novamente
+        n2 = -1*n2;
+        System.out.println(n2);
+    }
+}
