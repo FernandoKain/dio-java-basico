24f521d75aa67faddb46c7d99a732036235ab57d
FernandoKain <fernandomanga@hotmail.com>
2022-12-23 00:59:49 -0300

    Arquivo PalavrasReservadas foi adicionado

---
+++ java-anatomia-classes/src/com/fernando/palavrasreservadas/PalavrasReservadasEmJava.java
@@ -0,0 +1,57 @@
+package com.fernando.palavrasreservadas;
+
+public class PalavrasReservadasEmJava {
+    public static void main(String[] args) {
+        System.out.println("As palavras reservadas em JAVA são:" +
+        "\nAbstract" +
+        "\nAssert" +
+        "\nBoolean"+
+        "\nBreak"+
+        "\nByte"+
+        "\nCase"+
+        "\nCatch"+
+        "\nChar"+
+        "\nClass"+
+        "\nConst"+
+        "\nContinue"+
+        "\nDefault"+
+        "\nDo"+
+        "\nDouble"+
+        "\nElse"+
+        "\nEnum"+
+        "\nExtends"+
+        "\nFinal"+
+        "\nFinally"+
+        "\nFloat"+
+        "\nFor"+
+        "\nGoto"+
+        "\nIf"+
+        "\nImplements"+
+        "\nImport "+
+        "\nInstanceof"+
+        "\nInt"+
+        "\nInterfaces"+
+        "\nLong"+
+        "\nNative"+
+        "\nNew"+
+        "\nPackage"+
+        "\nPrivate"+
+        "\nProtected"+
+        "\nPublic"+
+        "\nReturn"+
+        "\nShort"+
+        "\nStatic"+
+        "\nStrictfp"+
+        "\nSuper"+
+        "\nSwitch"+
+        "\nSynchronized"+
+        "\nThis"+
+        "\nThrow"+
+        "\nThrows"+
+        "\nTransient"+
+        "\nTry"+
+        "\nVoid"+
+        "\nVolatile"+
+        "\nWhile");
+    }
+}
